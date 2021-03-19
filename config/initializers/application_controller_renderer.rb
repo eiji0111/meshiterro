f552c90580a35bb84a57757bd76f92eff940a362
eiji0111 <ee111fender@gmail.com>
2021-03-19 05:05:49 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ca75ce4f98ef4a7b75d33317df7a3c0481951488015cc650f41c793f3d084d60ea660993aa0e66abe194a44e5efd113e29ab9e4806d405cd9ec57113b09458a7'
